b820d683f107817476b774dfeaa8602eded68766
Nguyen Dang Khoa <wentallout@gmail.com>
2022-08-31 20:38:27 +0700

    remove hotjar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,23 +42,6 @@
 		href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
 		rel="stylesheet"
 	/>
-
-	<!-- Hotjar Tracking Code for https://www.wentallout.tech -->
-	<script>
-		(function (h, o, t, j, a, r) {
-			h.hj =
-				h.hj ||
-				function () {
-					(h.hj.q = h.hj.q || []).push(arguments);
-				};
-			h._hjSettings = { hjid: 3069496, hjsv: 6 };
-			a = o.getElementsByTagName('head')[0];
-			r = o.createElement('script');
-			r.async = 1;
-			r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-			a.appendChild(r);
-		})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-	</script>
 </svelte:head>
 
 <div class="page-container">
