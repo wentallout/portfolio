0e659fe5c3e41484f20c7643db777983051aee71
Nguyen Dang Khoa <wentallout@gmail.com>
2022-09-18 00:25:23 +0700

    update reza

--- src/routes/project/[slug]/+page.svelte
+++ src/routes/project/[slug]/+page.svelte
@@ -6,14 +6,19 @@
 	<title>{data.title}</title>
 </svelte:head>
 
-<article>
+<article class="info">
 	<h1 class="large-text">{data.title}</h1>
 	<span class="small-text date">Published: {data.date}</span>
 	<svelte:component this={data.content} />
 </article>
 
 <style>
+	.info {
+		display: block;
+	}
+
 	.date {
+		display: block;
 		font-weight: 300;
 	}
 </style>
