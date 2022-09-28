eba1761bbc95c25b2643b1fc4fb02cba8f94cc55
Nguyen Dang Khoa <wentallout@gmail.com>
2022-09-28 14:01:35 +0700

    tweaked Transition

--- src/lib/components/Common/Transition.svelte
+++ src/lib/components/Common/Transition.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#key url}
-	<div in:fly={{ y: -50, duration: 200, delay: 300 }} out:fly={{ y: -50, duration: 200 }}>
+	<div in:fly={{ y: -50, duration: 200 }}>
 		<slot />
 	</div>
 {/key}
