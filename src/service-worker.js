20ad2e13eaef26fbd40145574a06b3699b0660c2
Nguyen Dang Khoa <wentallout@gmail.com>
2022-10-24 11:51:30 +0700

    remove console.log from service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,20 +7,20 @@ const CACHE_NAME = `static-cache-${version}`;
 const to_cache = build.concat(files);
 
 worker.addEventListener('install', (event) => {
-	console.log('[ServiceWorker] Install');
+	// console.log('[ServiceWorker] Install');
 
 	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => {
-			console.log('[ServiceWorker] Pre-caching offline page');
+			// console.log('[ServiceWorker] Pre-caching offline page');
 			return cache.addAll(to_cache).then(() => {
 				worker.skipWaiting();
 			});
-		}),
+		})
 	);
 });
 
 worker.addEventListener('activate', (event) => {
-	console.log('[ServiceWorker] Activate');
+	// console.log('[ServiceWorker] Activate');
 	// Remove previous cached data from disk
 	event.waitUntil(
 		caches.keys().then(async (keys) =>
@@ -30,15 +30,15 @@ worker.addEventListener('activate', (event) => {
 						console.log('[ServiceWorker] Removing old cache', key);
 						return caches.delete(key);
 					}
-				}),
-			),
-		),
+				})
+			)
+		)
 	);
 	worker.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-	console.log('[ServiceWorker] Fetch', event.request.url);
+	// console.log('[ServiceWorker] Fetch', event.request.url);
 	if (event.request.mode !== 'navigate') {
 		return;
 	}
@@ -47,6 +47,6 @@ self.addEventListener('fetch', (event) => {
 			return caches.open(CACHE_NAME).then((cache) => {
 				return cache.match('offline.html');
 			});
-		}),
+		})
 	);
 });
