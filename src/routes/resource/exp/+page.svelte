73b46d175dda3d6ba591a07e7af373b3370efae5
Nguyen Dang Khoa <wentallout@gmail.com>
2023-04-12 23:23:44 +0700

    remove 3d stuff

--- src/routes/resource/exp/+page.svelte
+++ src/routes/resource/exp/+page.svelte
@@ -1,20 +1,7 @@
 <script>
-	import { Canvas } from '@threlte/core';
-	import { AmbientLight, OrbitControls, PerspectiveCamera } from '@threlte/core';
-	import { GLTF } from '@threlte/extras';
 </script>
 
-<div class="threejs">
-	<Canvas>
-		<PerspectiveCamera position={{ x: 5, y: 2, z: 5 }} fov={25}>
-			<OrbitControls autoRotate enableDamping />
-		</PerspectiveCamera>
-
-		<AmbientLight />
-
-		<GLTF url="/3d/plastic_monobloc_chair_01_4k.gltf" />
-	</Canvas>
-</div>
+<div class="threejs">Work in progress</div>
 
 <style>
 	.threejs {
