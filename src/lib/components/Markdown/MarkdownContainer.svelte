d53213e1281c9e9143080eab48389b0001d59c41
Nguyen Dang Khoa <wentallout@gmail.com>
2023-11-03 23:33:14 +0700

    temporarily remove folders

--- src/lib/components/Markdown/MarkdownContainer.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import '$lib/styles/markdown/markdown.css';
-</script>
-
-<div class="markdown">
-	<slot />
-</div>
--- src/lib/components/PWA/PWA.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import website from '$lib/config.js';
-	const { themeColor } = website;
-</script>
-
-<svelte:head>
-	<meta name="theme-color" content={themeColor} />
-</svelte:head>
