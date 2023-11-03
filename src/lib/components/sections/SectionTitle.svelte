7729b84c3590e05d290e066f541a3d298153c780
Nguyen Dang Khoa <wentallout@gmail.com>
2023-11-03 23:42:59 +0700

    move folders

---
+++ src/lib/components/sections/SectionTitle.svelte
@@ -0,0 +1,54 @@
+<script>
+	import SectionDesc from '$components/common/Section/SectionDesc.svelte';
+
+	export let sectionTitle = '';
+	export let sectionDesc = '';
+
+	let sectionIcon = {
+		color: 'var(--colorTextTertiary)',
+		height: 32,
+		width: 32
+	};
+</script>
+
+<div class="section">
+	<div class="section__title">
+		<div class="section__icon">
+			<slot {sectionIcon} />
+		</div>
+		<h2 class="section__text text-xl">{sectionTitle}</h2>
+	</div>
+
+	{#if sectionDesc !== ''}
+		<SectionDesc text={sectionDesc} />
+	{/if}
+</div>
+
+<style>
+	.section {
+		margin-bottom: var(--spaceXL);
+		width: 100%;
+	}
+
+	.section__title {
+		display: flex;
+		justify-content: left;
+
+		align-items: center;
+		gap: var(--spaceS);
+		margin-bottom: var(--spaceS);
+
+		position: relative;
+	}
+
+	.section__icon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.section__text {
+		font-family: var(--fontFancy);
+		color: var(--colorText);
+	}
+</style>
---
+++ src/lib/components/sections/project/SnippetList.svelte
@@ -0,0 +1,68 @@
+<script>
+	import CardListContainer from '$components/layout/container/CardListContainer.svelte';
+	import FigmaCard from '$components/resource/FigmaCard.svelte';
+
+	let snippetList = [
+		{
+			title: 'Pancake Stack Layout',
+			href: 'https://codepen.io/wentallout/pen/KKrqXMj',
+			desc: 'a commonly used layout in both desktop and mobile'
+		},
+		{
+			title: 'Sidebar Layout',
+			href: 'https://codepen.io/wentallout/pen/LYXLzLj',
+			desc: 'quick way to achieve a left sidebar + right main layout'
+		},
+		{
+			title: 'Holy Grail Layout',
+			href: 'https://codepen.io/wentallout/pen/yLQXzqe',
+			desc: 'classic holy grail layout using grid'
+		},
+		{
+			title: 'Responsive Hamburger Navbar without JS',
+			href: 'https://codepen.io/wentallout/pen/ExOmjXv',
+			desc: 'hamburger navbar that skip JS.'
+		},
+		{
+			title: 'Switcher',
+			href: 'https://codepen.io/wentallout/pen/WNYjKVd',
+			desc: 'typicial iOS style switcher with 2 state on/off'
+		},
+		{
+			title: 'Avatar',
+			href: 'https://codepen.io/wentallout/pen/dyQWjdx',
+			desc: 'avatar pure CSS'
+		},
+		{
+			title: 'Voting Bar',
+			href: 'https://codepen.io/wentallout/pen/dyQRzEv',
+			desc: 'voting element like reddit'
+		},
+		{
+			title: 'Panoramic Animation',
+			href: 'https://codepen.io/wentallout/pen/xxQEOzq',
+			desc: "animate an img from left to right when it's wider than its container"
+		},
+		{
+			title: 'Smooth Scroll',
+			href: 'https://codepen.io/wentallout/pen/ExNNvzR',
+			desc: 'smooth scroll with vanilla JS'
+		},
+		{
+			title: 'Glitchy Text Animation',
+			href: 'https://codepen.io/wentallout/pen/rNzBQOQ',
+			desc: 'futuristic glitchy text'
+		},
+		{
+			title: 'Clock',
+			href: 'https://codepen.io/wentallout/pen/rNQjbPW',
+			desc: 'a clock in VanillaJS'
+		}
+	];
+</script>
+
+<CardListContainer>
+	{#each snippetList as snippet}
+		<FigmaCard {...snippet} />
+	{/each}
+</CardListContainer>
