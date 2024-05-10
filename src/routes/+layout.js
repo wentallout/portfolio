d13e7d71d936539431db4a9c02631de8df345d20
Nguyen Dang Khoa <wentallout@gmail.com>
2024-05-11 00:01:00 +0700

    update blog titles

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,19 +1,18 @@
-import posthog from 'posthog-js'
-import { browser } from '$app/environment'
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
 
-export const prerender = 'auto'
+export const prerender = true;
 
 export function load({ url }) {
-  if (browser && import.meta.env.MODE !== 'development') {
-    posthog.init(
-		  'phc_mme4LlwanCuevCif1sYvl4RniLD8FhkzaAOmh1ArPX3',
-		  { api_host: 'https://us.i.posthog.com' },
-    )
-  }
+	if (browser && import.meta.env.MODE !== 'development') {
+		posthog.init('phc_mme4LlwanCuevCif1sYvl4RniLD8FhkzaAOmh1ArPX3', {
+			api_host: 'https://us.i.posthog.com'
+		});
+	}
 
-  const pathname = url.pathname
+	const pathname = url.pathname;
 
-  return {
-    pathname,
-  }
+	return {
+		pathname
+	};
 }
