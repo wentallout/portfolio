988b3ca1a8a9097f7deae9e4eb70e678ed830080
Nguyen Dang Khoa <wentallout@gmail.com>
2024-11-07 21:15:53 +0700

    add new arcjet related codes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,18 @@
 import { sequence } from '@sveltejs/kit/hooks';
 
+import { env } from '$env/dynamic/private';
+import arcjet, { shield } from '@arcjet/sveltekit';
+import { error, type RequestEvent } from '@sveltejs/kit';
+
 import { i18n } from '$lib/i18n.js';
 export const handle = i18n.handle(async (event, resolve) => {
 	const { url, request, locals } = event;
+
+	const decision = await aj.protect(event);
+
+	if (decision.isDenied()) {
+		return error(403, 'Forbidden');
+	}
 
 	// check for Referer header to know where the user is navigating from
 	const referer = request.headers.get('Referer');
@@ -17,6 +27,7 @@ export const handle = i18n.handle(async (event, resolve) => {
 		transformPageChunk: ({ html }) =>
 			html.replace('%splash-skip%', String(!!locals.internalReferer))
 	});
+	return resolve(event);
 });
 
 const PUBLIC_DOMAIN = 'wentallout.io.vn';
@@ -45,3 +56,14 @@ const directives = {
 // const csp = Object.entries(directives)
 // 	.map(([key, arr]) => key + ' ' + arr.join(' '))
 // 	.join('; ');
+
+// arcjet
+const aj = arcjet({
+	key: env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
+	rules: [
+		// Protect against common attacks with Arcjet Shield
+		shield({
+			mode: 'LIVE' // will block requests. Use "DRY_RUN" to log only
+		})
+	]
+});
