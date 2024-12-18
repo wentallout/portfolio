fc73621eb3e5e86f21b99307aefee872858a4a1f
Nguyen Dang Khoa <wentallout@gmail.com>
2024-12-19 00:09:19 +0700

    remove unusable things

--- src/routes/api/arcjet/+server.js
+++ src/routes/api/arcjet/+server.js
@@ -1,28 +1,5 @@
-import arcjet, { detectBot, shield, tokenBucket } from '@arcjet/sveltekit';
 import { error, json } from '@sveltejs/kit';
-
-const aj = arcjet({
-	characteristics: ['ip.src'], // Track requests by IP
-	key: process.env.ARCJET_KEY, // Get your site key from https://app.arcjet.com
-	rules: [
-		// Shield protects your app from common attacks e.g. SQL injection
-		shield({ mode: 'LIVE' }),
-		// Create a bot detection rule
-		detectBot({
-			// Block all bots except search engine crawlers. See
-			// https://arcjet.com/bot-list
-			allow: ['CATEGORY:SEARCH_ENGINE'],
-			mode: 'LIVE' // Blocks requests. Use "DRY_RUN" to log only
-		}),
-		// Create a token bucket rate limit. Other algorithms are supported.
-		tokenBucket({
-			capacity: 10, // Bucket capacity of 10 tokens
-			interval: 10, // Refill every 10 seconds
-			mode: 'LIVE',
-			refillRate: 5 // Refill 5 tokens per interval
-		})
-	]
-});
+import { aj } from '$lib/server/arcjet.js';
 
 export async function GET(event) {
 	const decision = await aj.protect(event, { requested: 5 }); // Deduct 5 tokens from the bucket
