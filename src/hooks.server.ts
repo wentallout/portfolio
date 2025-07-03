c21c270e1b0b216acdd5af352a0d2145862b091c
Nguyen Dang Khoa <wentallout@gmail.com>
2025-07-04 03:44:29 +0700

    feat: implement new bot blocker

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,17 @@
 import type { Handle } from '@sveltejs/kit';
+import { createHandler } from 'svelte-kit-bot-block'
 
 import { error, type RequestEvent } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event, {
-		preload: ({ type }) => {
-			return type === 'font' || type === 'js' || type === 'css';
-		}
-	});
+export const handle = createHandler();
 
-	// Continue with the route
-	return response;
-};
+// export const handle: Handle = async ({ event, resolve }) => {
+// 	const response = await resolve(event, {
+// 		preload: ({ type }) => {
+// 			return type === 'font' || type === 'js' || type === 'css';
+// 		}
+// 	});
+
+// 	// Continue with the route
+// 	return response;
+// };
