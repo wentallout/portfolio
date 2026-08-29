6b9f292f9320aceef14fff32bf268eeb0bdb26cf
Nguyen Dang Khoa <wentallout@gmail.com>
2026-08-29 16:45:59 +0700

    fix: remove bad icons

--- src/lib/assets/icons/icons.js
+++ src/lib/assets/icons/icons.js
@@ -4,83 +4,54 @@ import ArrowLineUp from '~icons/ph/arrow-line-up';
 import ArrowRight from '~icons/ph/arrow-right';
 import Article from '~icons/ph/article-ny-times';
 import At from '~icons/ph/at';
-import BoundingBox from '~icons/ph/bounding-box';
 import CalendarBlank from '~icons/ph/calendar-blank';
 import CaretDown from '~icons/ph/caret-down';
 import CaretRight from '~icons/ph/caret-right';
 import Clock from '~icons/ph/clock';
-import CodePen from '~icons/ph/codepen-logo';
 import Copy from '~icons/ph/copy';
 import CopySimple from '~icons/ph/copy-simple';
 import EnvelopeSimple from '~icons/ph/envelope-simple';
 import Folder from '~icons/ph/folder';
 import GitHubLogo from '~icons/ph/github-logo';
-import HandHeart from '~icons/ph/hand-heart';
 import House from '~icons/ph/house';
 import LinkedinLogo from '~icons/ph/linkedin-logo';
 import ListBullets from '~icons/ph/list-bullets';
 import MagnifyingGlass from '~icons/ph/magnifying-glass';
-import MonitorPlay from '~icons/ph/monitor-play';
 import Moon from '~icons/ph/moon';
 import PaperPlaneRight from '~icons/ph/paper-plane-right';
 import Pen from '~icons/ph/pen';
 import Phone from '~icons/ph/phone';
-import ProjectorScreen from '~icons/ph/projector-screen';
-import Question from '~icons/ph/question';
-import Star from '~icons/ph/star';
 import Sun from '~icons/ph/sun';
 import Tag from '~icons/ph/tag';
-import Toolbox from '~icons/ph/toolbox';
 import User from '~icons/ph/user';
-import Wrench from '~icons/ph/wrench';
 import X from '~icons/ph/x';
 
-import Compass from '~icons/ph/Compass';
-import Grid from '~icons/ph/Compass';
-import Sparkle from '~icons/ph/Sparkle';
-import Palette from '~icons/ph/Palette';
-import TextT from '~icons/ph/TextT';
-
 export {
-	TextT,
-	Palette,
-	Sparkle,
-	Grid,
-	Compass,
 	ArrowCounterClockwise,
 	ArrowLeft,
 	ArrowLineUp,
 	ArrowRight,
 	Article,
 	At,
-	BoundingBox,
 	CalendarBlank,
 	CaretDown,
 	CaretRight,
 	Clock,
-	CodePen,
 	Copy,
 	CopySimple,
 	EnvelopeSimple,
 	Folder,
 	GitHubLogo,
-	HandHeart,
 	House,
 	LinkedinLogo,
 	ListBullets,
 	MagnifyingGlass,
-	MonitorPlay,
 	Moon,
 	PaperPlaneRight,
 	Pen,
 	Phone,
-	ProjectorScreen,
-	Question,
-	Star,
 	Sun,
 	Tag,
-	Toolbox,
 	User,
-	Wrench,
 	X
 };
